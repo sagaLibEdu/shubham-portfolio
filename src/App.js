fe44de51a337509c4ebbd59076f9111af44122c1
LAPTOP-KPRNEMOP\As <shubhshubhon9@gmail.com>
2022-06-05 13:51:18 +0530

    json file path fix

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ class App extends Component {
     const load = document.getElementById('siteLoading');
 
     $.ajax({
-      url:'/resumeData.json',
+      url:'/shubham-portfolio/resumeData.json',
       dataType:'json',
       cache: false,
       success: function(data){
